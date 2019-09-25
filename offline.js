9f38c17697995e5181741df4e0edffbb6b346b91
semicon <30399464+semicon@users.noreply.github.com>
2019-09-25 18:41:14 +0700

    puzzle001
    puzzle001

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1569411481,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/spritefont.png",
+		"images/boxbase-sheet0.png",
+		"images/image-sheet0.png",
+		"images/boxtouch-sheet0.png",
+		"images/boxtouch-sheet1.png",
+		"images/boxtouch-sheet2.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
